45d91c4b6bb2a9c8f7e93a8c4b869ec8cdec0839
kharisma tara mukti <kharismataram@gmail.com>
2023-05-03 18:15:26 +0700

    update

--- js/cekStunting.js
+++ js/cekStunting.js
@@ -1,74 +1,74 @@
-document.getElementById("cekstunting1").addEventListener("click", function() {
-    event.preventDefault();
-    console.log("cekstunting");
-    const nama = document.getElementById("nama").value;
-    const jk = document.getElementById("jk").value;
-    const bb = parseInt(document.getElementById("bb").value);
-    const tl = new Date(document.getElementById("tl").value);
-    const umur = parseInt(document.getElementById("umur").value);
-    const tinggi = parseInt(document.getElementById("tinggi").value);
+document.getElementById("cekstunting1").addEventListener("click", function () {
+  event.preventDefault();
+  console.log("cekstunting");
+  const nama = document.getElementById("nama").value;
+  const jk = document.getElementById("jk").value;
+  const bb = parseInt(document.getElementById("bb").value);
+  const tl = new Date(document.getElementById("tl").value);
+  const umur = parseInt(document.getElementById("umur").value);
+  const tinggi = parseInt(document.getElementById("tinggi").value);
 
-    const tgl_lahir = tl.getDate();
-    const bln_lahir = tl.getMonth() + 1;
-    const thn_lahir = tl.getFullYear();
-    const today = new Date();
-    const umur_bulan = (today.getFullYear() - thn_lahir) * 12 + today.getMonth() - bln_lahir;
+  const tgl_lahir = tl.getDate();
+  const bln_lahir = tl.getMonth() + 1;
+  const thn_lahir = tl.getFullYear();
+  const today = new Date();
+  const umur_bulan = (today.getFullYear() - thn_lahir) * 12 + today.getMonth() - bln_lahir;
 
-    const zbb = Math.pow(bb / 6, 0.483);
-    const tbu = 61.4 + 3.191 * umur + 0.025 * Math.pow(umur, 2);
-    const ztb = (tinggi - tbu) / 4.083;
+  const zbb = Math.pow(bb / 6, 0.483);
+  const tbu = 61.4 + 3.191 * umur + 0.025 * Math.pow(umur, 2);
+  const ztb = (tinggi - tbu) / 4.083;
 
-    const stunting = ztb < -2 || umur_bulan >= 24 ? "tidak " : "iya";
+  const stunting = ztb < -2 && umur_bulan > 24 ? "tidak " : "iya";
 
-    // document.getElementById("namahasil").textContent = ` ${nama}`;
-    // document.getElementById("jkhasil").textContent = ` ${jk}`;
-    // document.getElementById("bbhasil").textContent = ` ${bb} kg`;
-    // document.getElementById("tthasil").textContent = ` ${tinggi} cm`;
-    // document.getElementById("stunting").textContent = ` ${stunting}`;
+  // document.getElementById("namahasil").textContent = ` ${nama}`;
+  // document.getElementById("jkhasil").textContent = ` ${jk}`;
+  // document.getElementById("bbhasil").textContent = ` ${bb} kg`;
+  // document.getElementById("tthasil").textContent = ` ${tinggi} cm`;
+  // document.getElementById("stunting").textContent = ` ${stunting}`;
 
-    let result = "";
-    result =
-        "<h2 >" +
-        " Hasil Perhitungan " +
-        "</h2>" +
-        "<h3>" +
-        "Nama = " +
-        nama +
-        "</h3>" +
-        "<h3>" +
-        "Jenis Kelamin = " +
-        jk +
-        "</h3>" +
-        "<h3>" +
-        "Berat Badan = " +
-        bb +
-        "</h3>" +
-        "<h3>" +
-        "Tanggal Lahir = " +
-        tl +
-        "</h3>" +
-        "<h3>" +
-        "umur = " +
-        umur +
-        "</h3>" +
-        "<h3>" +
-        "tinggi = " +
-        tinggi +
-        "</h3>" +
-        "<h3>" +
-        "Status = " +
-        stunting +
-        "</h3>";
-    document.getElementById("result").innerHTML = result;
-    document.getElementById("result").style.padding = "40px";
-    document.getElementById("result").style.textAlign = "start";
+  let result = "";
+  result =
+    "<h2 >" +
+    " Hasil Perhitungan " +
+    "</h2>" +
+    "<h3>" +
+    "Nama = " +
+    nama +
+    "</h3>" +
+    "<h3>" +
+    "Jenis Kelamin = " +
+    jk +
+    "</h3>" +
+    "<h3>" +
+    "Berat Badan = " +
+    bb +
+    "</h3>" +
+    "<h3>" +
+    "Tanggal Lahir = " +
+    tl +
+    "</h3>" +
+    "<h3>" +
+    "umur = " +
+    umur +
+    "</h3>" +
+    "<h3>" +
+    "tinggi = " +
+    tinggi +
+    "</h3>" +
+    "<h3>" +
+    "Status = " +
+    stunting +
+    "</h3>";
+  document.getElementById("result").innerHTML = result;
+  document.getElementById("result").style.padding = "40px";
+  document.getElementById("result").style.textAlign = "start";
 
-    // document.getElementById("namahasil").innerHTML = "Nama: " + nama;
-    // document.getElementById("jkhasil").innerHTML = "Jenis Kelamin: " + jk;
-    // document.getElementById("bbhasil").innerHTML = "Berat Badan: " + bb;
-    // document.getElementById("tthasil").innerHTML = "Tinggi Badan: " + tinggi;
-    // document.getElementById("stunting").innerHTML = "satutus " + stunting;
-    document.querySelector("#result").style.display = "block";
+  // document.getElementById("namahasil").innerHTML = "Nama: " + nama;
+  // document.getElementById("jkhasil").innerHTML = "Jenis Kelamin: " + jk;
+  // document.getElementById("bbhasil").innerHTML = "Berat Badan: " + bb;
+  // document.getElementById("tthasil").innerHTML = "Tinggi Badan: " + tinggi;
+  // document.getElementById("stunting").innerHTML = "satutus " + stunting;
+  document.querySelector("#result").style.display = "block";
 });
 // zbb: z-score untuk berat badan anak berdasarkan formula tertentu.
 // tbu: perkiraan tinggi badan anak berdasarkan umur.

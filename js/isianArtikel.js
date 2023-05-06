be57839292a6dff0c469eeddb197b1ba26e76e67
dwiwahyup <dwiwahyu1235@gmail.com>
2023-05-06 13:05:34 +0700

    update daftar artikel dan isian artikel

--- js/isianArtikel.js
+++ js/isianArtikel.js
@@ -34,15 +34,31 @@ fetch(`https://64539f69c18adbbdfea29dd5.mockapi.io/artikel/${id}`)
             '</div>' +
             '<div class="col-lg-10 isi-artikel">' +
 
-            '<p class="text-warna jenis-artikel">' + data["jenisArtikel"] + '</p>' +
-            '<h1 class="judul-artikel mt-3 ">' + data["judulArtikel"] + '</h1>' +
+            '<p class="text-warna jenis-artikel fw-semibold">' + data["jenisArtikel"] + '</p>' +
+            '<h1 class="judul-artikel mt-3 fw-semibold ">' + data["judulArtikel"] + '</h1>' +
             '<h6 class="tinjauan mt-3">Ditinjau oleh: <span class="text-warna"> dr. Fadhli Rizal Makarim </span> : 15 Juli 2021 </h6>' +
 
             '<div class="mt-3">' +
-            '<p class="text-justify"> ' + data["isiArtikel"] + '</p>' +
-            '<p class="text-justify"> ' + data["isiArtikel"] + '</p>' +
-            '<p class="text-justify"> ' + data["isiArtikel"] + '</p>' +
-            '<p class="text-justify"> ' + data["isiArtikel"] + '</p>' +
+            '<p class="text-justify"> ' + data["content1"] + '</p>' +
+            '<p class="text-justify"> ' + data["content2"] + '</p>' +
+            '<p class="text-justify"> ' + data["content3"] + '</p>' +
+            '<p class="text-justify"> ' + data["content4"] + '</p>' +
+
+            '<p class="text-justify"> ' + data["content1"] + '</p>' +
+            '<p class="text-justify"> ' + data["content2"] + '</p>' +
+            '<p class="text-justify"> ' + data["content3"] + '</p>' +
+            '<p class="text-justify"> ' + data["content4"] + '</p>' +
+
+            '<p class="text-justify"> ' + data["content1"] + '</p>' +
+            '<p class="text-justify"> ' + data["content2"] + '</p>' +
+            '<p class="text-justify"> ' + data["content3"] + '</p>' +
+            '<p class="text-justify"> ' + data["content4"] + '</p>' +
+
+            '<p class="text-justify"> ' + data["content1"] + '</p>' +
+            '<p class="text-justify"> ' + data["content2"] + '</p>' +
+            '<p class="text-justify"> ' + data["content3"] + '</p>' +
+            '<p class="text-justify"> ' + data["content4"] + '</p>' +
+
             '</div>' +
             '</div>';
         '</div>' +
@@ -52,3 +68,30 @@ fetch(`https://64539f69c18adbbdfea29dd5.mockapi.io/artikel/${id}`)
 
 
     });
+
+
+const articleList = document.querySelector("#article-list");
+
+fetch("https://64539f69c18adbbdfea29dd5.mockapi.io/artikel")
+    .then((response) => response.json())
+    .then((data) => {
+        data.forEach((article) => {
+            const articleCard = `
+          <div class="col-md-4 ">
+            <div class="card mb-4 shadow-sm mb-md-5" ">
+              <img src="${article.gambar}" class="card-img-top" alt="${article.judul}">
+              <div class="card-body artikel">
+                <h5 class="card-title jenis-artikel-card fs-4 fw-bold">${article.jenisArtikel}</h5>
+                <h4 class="card-title ">${article.judulArtikel}</h4>
+                <p class="card-text">${article.rangkuman}</p>
+                <div class="text-end linkaertikel">
+                  <a href="${article.selengkapnya}" class="btn fs-5 selengkapnya fw-bold">Selengkapnya</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        `;
+            articleList.innerHTML += articleCard;
+        });
+    })
+    .catch((error) => console.log(error));

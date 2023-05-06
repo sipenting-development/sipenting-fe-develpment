9ebf6fe926596b1d9792526ec8046807bab6b6f5
kharisma tara mukti <kharismataram@gmail.com>
2023-05-06 13:28:09 +0700

    update js fs

--- js/cekStunting.js
+++ js/cekStunting.js
@@ -306,14 +306,14 @@ fetch("https://64539f69c18adbbdfea29dd5.mockapi.io/artikel")
     data.forEach((article) => {
       const articleCard = `
           <div class="col-md-4 ">
-            <div class="card mb-4 shadow-sm mb-md-5" ">
+            <div class="card  mb-4 shadow-sm mb-md-5" ">
               <img src="${article.gambar}" class="card-img-top" alt="${article.judul}">
               <div class="card-body ">
-                <h5 class="card-title jenis-artikel fs-4 fw-bold">${article.jenisArtikel}</h5>
+                <h5 class="card-title jenis-artikel fs-7 fw-semibold">${article.jenisArtikel}</h5>
                 <h4 class="card-title ">${article.judulArtikel}</h4>
                 <p class="card-text">${article.rangkuman}</p>
                 <div class="text-end linkaertikel">
-                  <a href="${article.selengkapnya}" class="btn fs-5 selengkapnya fw-bold">Selengkapnya</a>
+                  <a href="${article.selengkapnya}" class="btn fs-5 selengkapnya fw-semibold">Selengkapnya</a>
                 </div>
               </div>
             </div>

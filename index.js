43741439d236dbc65a9d4c43f7adf9c8217cefc4
WhataD?0wD <ditakusuma66@gmail.com>
2023-05-06 13:28:12 +0700

    edit card artikel

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ fetch("https://64539f69c18adbbdfea29dd5.mockapi.io/artikel")
             <div class="card mb-4 shadow-sm mb-md-5" ">
               <img src="${article.gambar}" class="card-img-top" alt="${article.judul}">
               <div class="card-body artikel ">
-                <h5 class="card-title jenis-artikel fs-4 fw-bold">${article.jenisArtikel}</h5>
+                <h5 class="card-title jenis-artikel fs-7 fw-semibold">${article.jenisArtikel}</h5>
                 <h4 class="card-title ">${article.judulArtikel}</h4>
                 <p class="card-text">${article.rangkuman}</p>
                 <div class="text-end linkaertikel">
-                  <a href="${article.selengkapnya}" class="btn fs-5 selengkapnya fw-bold">Selengkapnya</a>
+                  <a href="${article.selengkapnya}" class="btn fs-5 selengkapnya fw-semibold">Selengkapnya</a>
                 </div>
               </div>
             </div>

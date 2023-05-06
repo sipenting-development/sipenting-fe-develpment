70b930c946834d5877e8501a9bb37aa216fc10d2
kharisma tara mukti <kharismataram@gmail.com>
2023-05-06 15:46:49 +0700

    selesai

--- js/isianArtikel.js
+++ js/isianArtikel.js
@@ -8,68 +8,95 @@ fetch(`https://64539f69c18adbbdfea29dd5.mockapi.io/artikel/${id}`)
   .then((data) => {
     // console.log(data);
 
-    temp = document.createElement('div');
-    temp.className = 'results';
+    temp = document.createElement("div");
+    temp.className = "results";
 
     temp.innerHTML =
-
       '<div class="hero-image">' +
-      '<img class="img-fluid " src="' + data["gambar"] + '"> ' +
-      '</div>' +
-
+      '<img class="img-fluid " src="' +
+      data["gambar"] +
+      '"> ' +
+      "</div>" +
       '<div class="container mt-5">' +
       '<div class="row">' +
       '<div class="col-lg-1">' +
       '<div class="d-flex flex-lg-column justify-content icon-artikel mb-3 gap-3 ">' +
       '<a href="# " class="me-3 text-decoration-none">' +
       '<i class="bi bi-facebook "></i>' +
-      '</a>' +
+      "</a>" +
       '<a href="# " class="me-3 text-decoration-none">' +
       '<i class="bi bi-instagram "></i>' +
-      '</a>' +
+      "</a>" +
       '<a href="# " class="text-decoration-none">' +
       '<i class="bi bi-whatsapp "></i>' +
-      '</a>' +
-      '</div>' +
-      '</div>' +
+      "</a>" +
+      "</div>" +
+      "</div>" +
       '<div class="col-lg-10 isi-artikel">' +
-
-      '<p class="text-warna jenis-artikel fw-semibold">' + data["jenisArtikel"] + '</p>' +
-      '<h1 class="judul-artikel mt-3 fw-semibold ">' + data["judulArtikel"] + '</h1>' +
+      '<p class="text-warna jenis-artikel fw-semibold">' +
+      data["jenisArtikel"] +
+      "</p>" +
+      '<h1 class="judul-artikel mt-3 fw-semibold ">' +
+      data["judulArtikel"] +
+      "</h1>" +
       '<h6 class="tinjauan mt-3">Ditinjau oleh: <span class="text-warna"> dr. Fadhli Rizal Makarim </span> : 15 Juli 2021 </h6>' +
-
       '<div class="mt-3">' +
-      '<p class="text-justify"> ' + data["content1"] + '</p>' +
-      '<p class="text-justify"> ' + data["content2"] + '</p>' +
-      '<p class="text-justify"> ' + data["content3"] + '</p>' +
-      '<p class="text-justify"> ' + data["content4"] + '</p>' +
+      '<p class="text-justify"> ' +
+      data["content1"] +
+      "</p>" +
+      '<p class="text-justify"> ' +
+      data["content2"] +
+      "</p>" +
+      '<p class="text-justify"> ' +
+      data["content3"] +
+      "</p>" +
+      '<p class="text-justify"> ' +
+      data["content4"] +
+      "</p>" +
+      '<p class="text-justify"> ' +
+      data["content1"] +
+      "</p>" +
+      '<p class="text-justify"> ' +
+      data["content2"] +
+      "</p>" +
+      '<p class="text-justify"> ' +
+      data["content3"] +
+      "</p>" +
+      '<p class="text-justify"> ' +
+      data["content4"] +
+      "</p>" +
+      '<p class="text-justify"> ' +
+      data["content1"] +
+      "</p>" +
+      '<p class="text-justify"> ' +
+      data["content2"] +
+      "</p>" +
+      '<p class="text-justify"> ' +
+      data["content3"] +
+      "</p>" +
+      '<p class="text-justify"> ' +
+      data["content4"] +
+      "</p>" +
+      '<p class="text-justify"> ' +
+      data["content1"] +
+      "</p>" +
+      '<p class="text-justify"> ' +
+      data["content2"] +
+      "</p>" +
+      '<p class="text-justify"> ' +
+      data["content3"] +
+      "</p>" +
+      '<p class="text-justify"> ' +
+      data["content4"] +
+      "</p>" +
+      "</div>" +
+      "</div>";
+    "</div>" + "</div>";
 
-      '<p class="text-justify"> ' + data["content1"] + '</p>' +
-      '<p class="text-justify"> ' + data["content2"] + '</p>' +
-      '<p class="text-justify"> ' + data["content3"] + '</p>' +
-      '<p class="text-justify"> ' + data["content4"] + '</p>' +
-
-      '<p class="text-justify"> ' + data["content1"] + '</p>' +
-      '<p class="text-justify"> ' + data["content2"] + '</p>' +
-      '<p class="text-justify"> ' + data["content3"] + '</p>' +
-      '<p class="text-justify"> ' + data["content4"] + '</p>' +
-
-      '<p class="text-justify"> ' + data["content1"] + '</p>' +
-      '<p class="text-justify"> ' + data["content2"] + '</p>' +
-      '<p class="text-justify"> ' + data["content3"] + '</p>' +
-      '<p class="text-justify"> ' + data["content4"] + '</p>' +
-
-      '</div>' +
-      '</div>';
-    '</div>' +
-      '</div>';
-
-    document.getElementsByClassName('isi-artikel')[0].appendChild(temp);
-
-
+    document.getElementsByClassName("isi-artikel")[0].appendChild(temp);
   });
 
-const articleList = document.querySelector("#article-list");
+let articleList = document.querySelector("#article-list");
 
 fetch("https://64539f69c18adbbdfea29dd5.mockapi.io/artikel")
   .then((response) => response.json())
